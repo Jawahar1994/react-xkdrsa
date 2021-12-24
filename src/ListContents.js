f88188159cfc9a43c46d5084d1dd44eab3efcb73
Jawahar1994 <jawahar.sharma.234@gmail.com>
2021-12-24 09:26:47 +0530

    1 file modified, 1 added, 1 deleted

--- src/ListContents.js
+++ src/ListContents.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import user from './user.jpg';
 
-export default function ListContents() {
+export default function ListContents(props) {
+  const contents = props.contents;
+
+  const listContents = content.map((content) => {
+    return (
+      <div className="content-info">
+        <div className="content-image">
+          <img src={user} alt={content.name} />
+        </div>
+        <div className="content-caption">
+          <h3>{content.name}</h3>
+          <p>{content.email}</p>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <>
       <h3>List Content </h3>
       <hr />
-      <div className="content-list"></div>
+      <div className="content-list">{listContents}</div>
     </>
   );
 }
